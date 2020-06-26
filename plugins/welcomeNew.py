730fafa1d7de877f4316c70ce3015e2ce94b5355
LoveCany <admin@lovecany.me>
2020-06-26 21:10:38 +0900

    update welcome plugin

--- plugins/welcomeNew.py
+++ plugins/welcomeNew.py
@@ -9,6 +9,6 @@ sys.path.append('..')
 @on_notice('group_increase')
 async def _(session: NoticeSession):
     if session.event.group_id == config.GROUP_ID:
-        await session.send(f'欢迎新成员{session.event.user_id}加入ReoNa应援团！请认真阅读置顶群公告。\n'
+        await session.send(f'欢迎新成员[CQ:at,qq={session.event.user_id}]加入ReoNa应援团！请认真阅读置顶群公告。\n'
                            f'相关音视频和图像资源已上传到群文件、群相册和资源站中，'
                            f'在取用的同时请遵守未经许可不得向外转载的规定，感谢配合。')
